d1fdbf156642ff4deb37452685c5f9399383a48b
kanishkaverma <kanisverma@gmail.com>
2021-05-16 06:04:35 +0530

    added bubble sort feature and tests for it.

--- tests.py
+++ tests.py
@@ -1,13 +1,15 @@
 import unittest
 from insertion_sort import insertion_sort
+from bubble_sort import bubble_sort
 import random
 
 
 
 integer_list = list(range(1000)) 
 random.shuffle(integer_list) 
-sorted_list = integer_list
+ 
 #sorted list for tests 
+sorted_list = integer_list
 sorted_list.sort()
 
 class Test_sort(unittest.TestCase):
@@ -15,6 +17,10 @@ class Test_sort(unittest.TestCase):
     """Test for insertion sort""" 
     def test_insertion_sort(self):
         self.assertEqual(insertion_sort(integer_list), sorted_list, "Test insertion sort")
+
+    """Test for bubble sort""" 
+    def test_bubble_sort(self):
+        self.assertEqual(bubble_sort(integer_list), sorted_list, "Test bubble sort")
 
 
 if __name__ == "__main__": 
